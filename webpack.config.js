f54550d641593c7ee221e99da8579a299c56a253
GreysonR <44911101+GreysonR@users.noreply.github.com>
2024-02-19 18:36:39 -0600

    Remove github actions

--- webpack.config.js
+++ webpack.config.js
@@ -5,7 +5,7 @@ module.exports = {
 	mode: "development",
 	entry: path.resolve(__dirname, "src/ter.js"),
 	output: {
-		path: path.resolve(__dirname, "dist"),
+		path: path.resolve(__dirname, "dev_dist"),
 		filename: "ter.js",
 		clean: false,
 
@@ -21,7 +21,7 @@ module.exports = {
 	devtool: "source-map",
 	devServer: {
 		static: {
-			directory: path.resolve(__dirname, "dist"),
+			directory: path.resolve(__dirname, "dev_dist"),
 		},
 		port: 80,
 		open: true,
